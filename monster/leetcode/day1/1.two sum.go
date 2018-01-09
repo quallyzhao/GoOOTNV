192b966527ed3a8d87f0ea432d5affb939742bfa
litao <TeamMicLee@foxmail.com>
2018-01-09 14:29:01 +0800

    添加练习描述

--- monster/leetcode/day1/1.two sum.go
+++ monster/leetcode/day1/1.two sum.go
@@ -1,3 +1,11 @@
+/*
+* @Description: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+*               给定一个整数数组, 将两个数字相加得到特定的目标, 返回这两个数字的索引。
+* @Example: Given nums = [2, 7, 11, 15], target = 9,
+*           Because nums[0] + nums[1] = 2 + 7 = 9,
+*           return [0, 1].
+*/
+
 func twoSum(nums []int, target int) []int {
 	lenNums := len(nums)
 	for i := 0; i < lenNums; i++ {
