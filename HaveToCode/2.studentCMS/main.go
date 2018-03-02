7c2e564ae80c263004347034f4a0847ed54f604c
zhou <z@zhous-MacBook-Pro.local>
2018-03-02 10:57:05 +0800

    add StudentCMS

---
+++ HaveToCode/2.studentCMS/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Student struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+type StudentSet struct{
+	M map[string]*Student `json:"data"`
+}
+
+func main(){
+	studentSet := NewStudentSets()
+	studentSet.Add(1,"zhouchengbin")
+	fmt.Println(studentSet.list())
+	Dump("test.txt",studentSet)
+}
+// 初始化学生Map
+func NewStudentSets()*StudentSet{
+	return &StudentSet{M:make(map[string]*Student,0)}
+}
+func (s *StudentSet)Add(id int ,name string)(err error){
+	_ ,ok := s.M[name]
+	if ok{
+		err = fmt.Errorf("student %s already exists.",name)
+		return
+	}
+	s.M[name] = &Student{id,name}
+	return
+}
+
+func(s*StudentSet)list() string{
+	var str string = "Id\t\tName\n"
+	for k,v := range s.M{
+		str += fmt.Sprintf("%d\t\t%s",v.Id,k)
+	}
+	return str
+}
+// Remove student that specified
+func (s*StudentSet)remove(name string)error{
+	_ ,ok := s.M[name]
+	if ok {
+		delete(s.M,name)
+		return nil
+	}
+	return fmt.Errorf("not found")
+}
+// Clear all students info
+func (s*StudentSet)clear(){
+	s.M = make(map[string]*Student,0)
+}
+// String implements String method
+func (s *StudentSet) String() string {
+	return s.list()
+}
+// Dump students info to the file that specified
+func Dump(fileName string, stu *StudentSet) (err error) {
+	fd, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer fd.Close()
+
+	bs, err := json.Marshal(stu)
+	if err != nil {
+		return
+	}
+
+	_, err = fd.Write(bs)
+	if err != nil {
+		return
+	}
+
+	return
+}
+// Load students info to the file that specified
+func Load(fileName string) (stu *StudentSet, err error) {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(bs, &stu)
+	if err != nil {
+		return
+	}
+
+	return
+}
